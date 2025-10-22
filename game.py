d7345e818e13d6ccd8b98815738192f91cdf5de1
VadimVibe <kolesnikov.vadim2007@gmail.com>
2025-10-22 07:54:51 +0300

    Функция показа изначального поля и номеров ячеек

--- game.py
+++ game.py
@@ -4,6 +4,13 @@ def print_place():
     print(place[4], '|', place[5], '|', place[6])
     print('--+---+--')
     print(place[7], '|', place[8], '|', place[9])
+def print_play_game():
+    print("Игровое поле с номерами ячеек")
+    print(' 1 | 2 | 3 ')
+    print('---+---+---')
+    print(' 4 | 5 | 6 ')
+    print('---+---+---')
+    print(' 7 | 8 | 9 ')
 
 place,hod_pl,hod, hod_zn = [' ']*10,['первый','второй','третий','четвертый','пятый'], 0, ['О','X']
 name = ['Игрок 1','Игрок 2']
