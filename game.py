42e6b823c09fa4e420949f1a7ddcff5eccfe1611
VadimVibe <kolesnikov.vadim2007@gmail.com>
2025-10-22 07:54:04 +0300

    Функция показа поля

--- game.py
+++ game.py
@@ -1,2 +1,9 @@
+def print_place():
+    print(place[1], '|', place[2], '|', place[3])
+    print('--+---+--')
+    print(place[4], '|', place[5], '|', place[6])
+    print('--+---+--')
+    print(place[7], '|', place[8], '|', place[9])
+
 place,hod_pl,hod, hod_zn = [' ']*10,['первый','второй','третий','четвертый','пятый'], 0, ['О','X']
 name = ['Игрок 1','Игрок 2']
